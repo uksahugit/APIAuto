d928835110026b82a95c9f63ca2678472c57325e
uksahugit <106351827+uksahugit@users.noreply.github.com>
2022-05-30 17:18:10 +0530

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -8,11 +8,6 @@ import java.io.File;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, features = "src/test/resources/features/DemoAPIAutomation.feature",glue = "steps")
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"}, features = "src/test/resources/features/DemoAPIAutomation.feature",glue = "steps")
 public class TestRunner {
-    @AfterClass
-    public static void writeExtentReport() {
-        Reporter.loadXMLConfig(new File("C:/Projects/RestAssured+Cucumber+Azure/extent-config.xml"));
-
-    }
 }
